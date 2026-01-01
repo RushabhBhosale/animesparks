55aca3bf295a3d5e499ef3d5d9757de1b7cf0072
Rushabh <145191870+RushabhBhosale@users.noreply.github.com>
2026-01-01 18:53:48 +0530

    minor update

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -152,7 +152,7 @@ export default async function BlogDetailPage({
         <div className="border-b border-gray-200 py-4">
           <div className="flex items-center gap-2 text-sm">
             <Link
-              href="/"
+              href="/home"
               className="font-semibold text-gray-600 hover:text-red-600 transition-colors"
             >
               Home
